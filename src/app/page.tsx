afd094510854d17c6f83b2d4092d1e27916abdae
noeyislearning <hello@noeyislearning.dev>
2024-08-22 00:59:29 +0800

    refactor: add new text, change text color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import DatasetsTable from "@/components/DatasetsTable"
 
 export default function Home() {
   return (
-    <div className="text-white">
-      <h1 className="text-4xl text-center">Datasets</h1>
-
+    <div className="text-blue-800">
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl text-center">Datasets</h1>
+        <span className="text-xs">Collected by Cogxen</span>
+      </div>
       <div className="pt-16 mx-auto w-full max-w-7xl">
         <DatasetsTable />
       </div>
