e559828d3c2b23681636c7ee9465158d0032f9a8
noeyislearning <hello@noeyislearning.dev>
2024-08-22 10:13:33 +0800

    style: update footer's padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Footer() {
   return (
-    <footer className="px-4 pt-8 w-full mx-auto text-sm text-slate-400">
+    <footer className="px-4 pt-16 w-full mx-auto text-sm text-slate-400">
       <div className="flex flex-col gap-2 items-center">
         This is a curated collection of publicly available datasets designed to support student
         experimentation and data analysis projects.
